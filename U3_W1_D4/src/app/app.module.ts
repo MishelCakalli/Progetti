7f06822152f6bf2b08808e9429a1e6773dd3cff3
mishel <mishelcakalli@gmail.com>
2024-03-21 15:38:42 +0100

    commit aggiorna pagine

--- U3_W1_D4/src/app/app.module.ts
+++ U3_W1_D4/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -7,6 +8,23 @@ import { EvidenceComponent } from './components/evidence/evidence.component';
 import { RelatedComponent } from './components/related/related.component';
 import { AdviseComponent } from './components/advise/advise.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ActivePostsComponent } from './components/active-posts/active-posts.component';
+import { InactivePostsComponent } from './components/inactive-posts/inactive-posts.component';
+const routes: Route[] = [
+  {
+      path: '',
+      component: HomeComponent,
+  },
+  {
+      path: 'activePosts',
+      component: ActivePostsComponent,
+  },
+  {
+      path: 'inactivePosts',
+      component: InactivePostsComponent,
+  },
+  
+];
 
 @NgModule({
   declarations: [
@@ -15,11 +33,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     EvidenceComponent,
     RelatedComponent,
     AdviseComponent,
-    NavbarComponent
+    NavbarComponent,
+    ActivePostsComponent,
+    InactivePostsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
