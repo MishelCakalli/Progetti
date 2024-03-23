3aebb646f538951aa906c8e42434ca81a9a39f48
mishel <mishelcakalli@gmail.com>
2024-03-23 10:57:39 +0100

    commit pagina finita delle macchine

--- U3_W1_D5_ProgettoSettimanaleAngular/src/app/components/audi/audi.component.ts
+++ U3_W1_D5_ProgettoSettimanaleAngular/src/app/components/audi/audi.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post.interface';
 
 @Component({
   selector: 'app-audi',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./audi.component.scss']
 })
 export class AudiComponent {
+  posts: Post[] = [];
+  randomCars: Post[] = [];
 
+
+  constructor() {}
+
+  ngOnInit() {
+    this.evidencePost();
+  }
+
+  async evidencePost() {
+    try {
+      const response = await fetch('../../assets/db.json');
+      const data: Post[] = await response.json();
+      this.posts = data;
+      this.randomCars = this.extractRandomItems(data, 2);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  extractRandomItems(array: Post[], count: number): Post[] {
+    const shuffled = array.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
+  }
+
+  
 }
