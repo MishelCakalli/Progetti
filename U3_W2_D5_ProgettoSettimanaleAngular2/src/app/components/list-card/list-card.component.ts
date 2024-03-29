fd28a21554d2216040000617b31b20f85e5be5ea
mishel <mishelcakalli@gmail.com>
2024-03-29 14:49:20 +0100

    commit home finita

--- U3_W2_D5_ProgettoSettimanaleAngular2/src/app/components/list-card/list-card.component.ts
+++ U3_W2_D5_ProgettoSettimanaleAngular2/src/app/components/list-card/list-card.component.ts
@@ -2,7 +2,9 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/todo.interface';
+import { User } from 'src/app/models/user.interface';
 import { TodoService } from 'src/app/services/todo.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-list-card',
@@ -11,15 +13,23 @@ import { TodoService } from 'src/app/services/todo.service';
 })
 export class ListCardComponent implements OnInit {
   todos: Todo[] = [];
+  users: User[] = [];
 
-  constructor(private todoService: TodoService) { }
+  constructor(
+    private todoService: TodoService,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
     this.todos = this.todoService.todos;
-    console.log(this.todos);
+    this.users = this.userService.users;
   }
 
   checkBox(todo: Todo) {
     todo.completed = !todo.completed;
+  }
+
+  cercaUser(userId: number): User | undefined {
+    return this.users.find(user => user.id === userId);
   }
 }
