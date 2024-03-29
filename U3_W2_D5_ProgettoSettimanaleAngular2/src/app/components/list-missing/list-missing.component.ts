86da36f297708338ed494ee0ac8dba126f2ee9a9
mishel <mishelcakalli@gmail.com>
2024-03-29 15:04:52 +0100

    commit pagina todo da fare

--- U3_W2_D5_ProgettoSettimanaleAngular2/src/app/components/list-missing/list-missing.component.ts
+++ U3_W2_D5_ProgettoSettimanaleAngular2/src/app/components/list-missing/list-missing.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo.interface';
+import { TodoService } from 'src/app/services/todo.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-list-missing',
   templateUrl: './list-missing.component.html',
   styleUrls: ['./list-missing.component.scss']
 })
-export class ListMissingComponent {
+export class ListMissingComponent implements OnInit {
+  incompleteTodos: Todo[] = [];
 
+  constructor(private todoService: TodoService, private userService: UserService) { }
+
+  ngOnInit() {
+    this.incompleteTodos = this.todoService.todos.filter(todo => !todo.completed);
+  }
+
+  checkBox(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  cercaUser(userId: number) {
+    return this.userService.users.find(user => user.id === userId);
+  }
 }
