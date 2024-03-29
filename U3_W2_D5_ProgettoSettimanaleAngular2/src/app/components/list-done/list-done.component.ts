f06df7403b57cb8939bb96227a5ef269d4124b14
mishel <mishelcakalli@gmail.com>
2024-03-29 15:02:57 +0100

    commit to do fatti

--- U3_W2_D5_ProgettoSettimanaleAngular2/src/app/components/list-done/list-done.component.ts
+++ U3_W2_D5_ProgettoSettimanaleAngular2/src/app/components/list-done/list-done.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo.interface';
+import { TodoService } from 'src/app/services/todo.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-list-done',
   templateUrl: './list-done.component.html',
   styleUrls: ['./list-done.component.scss']
 })
-export class ListDoneComponent {
+export class ListDoneComponent implements OnInit {
+  completedTodos: Todo[] = [];
 
+  constructor(private todoService: TodoService, private userService: UserService) { }
+
+  ngOnInit() {
+    this.completedTodos = this.todoService.todos.filter(todo => todo.completed);
+  }
+
+  checkBox(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  cercaUser(userId: number) {
+    return this.userService.users.find(user => user.id === userId);
+  }
 }
