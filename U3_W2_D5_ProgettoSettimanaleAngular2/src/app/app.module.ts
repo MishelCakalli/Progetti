ec62b9a1d4747de8d807bd4b61d1f377ec5c7e1f
mishel <mishelcakalli@gmail.com>
2024-03-29 11:57:36 +0100

    commit iniziale

--- U3_W2_D5_ProgettoSettimanaleAngular2/src/app/app.module.ts
+++ U3_W2_D5_ProgettoSettimanaleAngular2/src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListDoneComponent } from './components/list-done/list-done.component';
+import { ListMissingComponent } from './components/list-missing/list-missing.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { ListCardComponent } from './components/list-card/list-card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const routes: Route[] = [
+  {
+      path: '',
+      component: HomeComponent,
+  },
+  {
+      path: 'listDone',
+      component: ListDoneComponent,
+  },
+  {
+      path: 'listMissing',
+      component: ListMissingComponent,
+  },
+  {
+      path: 'userPage',
+      component: UserPageComponent,
+  },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListDoneComponent,
+    ListMissingComponent,
+    UserPageComponent,
+    ListCardComponent,
+    NavbarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ U3_W2_D5_ProgettoSettimanaleAngular2/src/app/components/list-done/list-done.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-done',
+  templateUrl: './list-done.component.html',
+  styleUrls: ['./list-done.component.scss']
+})
+export class ListDoneComponent {
+
+}
