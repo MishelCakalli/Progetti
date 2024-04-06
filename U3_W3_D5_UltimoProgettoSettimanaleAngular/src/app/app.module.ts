8c33fabe885bf157284940d5af13108e4dc1bf95
mishel <mishelcakalli@gmail.com>
2024-04-06 13:23:49 +0200

    commit

--- U3_W3_D5_UltimoProgettoSettimanaleAngular/src/app/app.module.ts
+++ U3_W3_D5_UltimoProgettoSettimanaleAngular/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
