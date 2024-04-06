58c1083b89f2010ed8a9c8c729c0b890cc6e8822
mishel <mishelcakalli@gmail.com>
2024-04-06 19:57:29 +0200

    commit

--- U3_W3_D5_UltimoProgettoSettimanaleAngular/src/app/app.module.ts
+++ U3_W3_D5_UltimoProgettoSettimanaleAngular/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +10,24 @@ import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { RegistratiComponent } from './components/registrati/registrati.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { UtentiComponent } from './components/utenti/utenti.component';
+import { ProfiloComponent } from './components/profilo/profilo.component';
+
+const routes: Route[] = [
+  {
+      path: '',
+      component: HomeComponent,
+  },
+  {
+      // path: 'activePosts',
+      // component: ActivePostsComponent,
+      // canActivate: [AuthGuard]
+  },
+
+];
+
 
 @NgModule({
   declarations: [
@@ -14,11 +35,16 @@ import { RegisterComponent } from './auth/register/register.component';
     HomeComponent,
     NavbarComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    RegistratiComponent,
+    MovieComponent,
+    UtentiComponent,
+    ProfiloComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ U3_W3_D5_UltimoProgettoSettimanaleAngular/src/app/components/accedi/accedi.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accedi',
+  templateUrl: './accedi.component.html',
+  styleUrls: ['./accedi.component.scss']
+})
+export class AccediComponent {
+
+}
